7579bdffd32a74cfb7c2d49410bee533b3d7a195
csPython <cspython@AD25TF04NGF1J-59.local>
2019-02-12 16:23:34 -0500

    Added 2nd shape (Square).

--- shapes5.py
+++ shapes5.py
@@ -1,11 +1,16 @@
 from graphics import*
 
+
 window = GraphWin("shapes5.py", 300, 300)
 window.setCoords(0,0, 1000, 1000)
 
-btri = Polygon(Point(100, 100), Point(150, 200), Point(200,100))
-btri.setFill(color_rgb(30,30, 230))
-btri.draw(window)
+triB = Polygon(Point(100, 100), Point(150, 200), Point(200,100))
+triB.setFill(color_rgb(30,30, 230))
+triB.draw(window)
+
+sqR = Rectangle(Point(900,100), Point( 800,200))
+sqR.setFill(color_rgb(255, 0,0))
+sqR.draw(window)
 
 
 
